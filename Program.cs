a9f764d628c0a6cd45cdcf6cc9eaf6a90debd949
Sachin Salanke <sachin.salanke@gmail.com>
2021-03-17 16:32:58 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minsk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
